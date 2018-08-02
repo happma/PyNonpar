56928c77eee85472a003017543bd18b4c7c3bd9e
Martin Happ <martinhapp@aon.at>
2018-08-02 23:53:12 +0200

    added Hettmansperger-Norton test

--- PyNonpar/hettmansperger.py
+++ PyNonpar/hettmansperger.py
@@ -10,6 +10,10 @@
 
 import pandas as pd
 import numpy as np
+import PyNonpar.pseudorank as ps
+import math
+import scipy
+from scipy.stats import norm
 
 def hettmansperger_norton_test(data, group, alternative, trend):
     """
@@ -25,14 +29,60 @@ def hettmansperger_norton_test(data, group, alternative, trend):
         float test statistic
     """
 
-    d = {'data': data, 'grp': group}
+    d = {'data': ps.psrank(data, group, ties_method = "average"), 'grp': group}
     df = pd.DataFrame(data=d)
-    df["grp"] = df["grp"].astype('category')
+    df['grp'] = df['grp'].astype(pd.api.types.CategoricalDtype(categories=np.unique(df['grp'].tolist()), ordered=True))
 
-    dff = df.sort_values(['data'])
+    df = df.sort_values(['grp'])
     # dff.assign(grp = dff['grp'][dff.index].values)
-    orig_sort = dff.index
-    dff = dff.reset_index(drop=True)
-    dff['grp'] = dff['grp'].cat.codes
+    # orig_sort = df.index
+    # df = df.reset_index(drop=True)
+    df['codes'] = df['grp'].cat.codes
 
-    return 0
+    # calculate group sizes and number of groups
+    N = len(df['data'])
+    a = len(np.unique(df['codes']))
+    n = [0 for x in range(a)]
+    p_hat = [0 for x in range(a)]
+
+    for i in range(0, a):
+        tmp = df[df['codes'] == i]
+        n[i] = len(tmp)
+        p_hat[i] = 1/N*(np.mean(tmp['data']) - 0.5)
+
+    # define weight function for hypothesis testing
+        w = np.arange(1, (a + 1))
+
+    if alternative == "decreasing":
+        w = w[::-1]
+    elif alternative == "custom":
+        w = trend
+
+    # defining matrices and vectors for computing test statistic
+    v1 = [1 for x in range(a)]
+    mS = np.diag(n)
+    mI = np.diag(v1)
+    mJ = np.outer(v1, v1)
+
+    W = np.dot( mS, ( mI - np.dot( 1/N*mJ, mS ) )  )
+
+    # variance estimator
+    sum_psranks_squared = 0
+    for i in range(N):
+        sum_psranks_squared += (df['data'].tolist()[i] - (N + 1)/2) ** 2
+    sum_psranks_squared = (1/N **2)*1/(N-1)*sum_psranks_squared
+
+    sigma_part = np.dot(W, w)
+    sigma_hat_squared = N*sum_psranks_squared*np.dot(np.dot( sigma_part.transpose(), np.linalg.inv(mS) ), sigma_part )
+
+    # test statistic
+    test_hettmansperger = math.sqrt(N)*np.dot(sigma_part.transpose(), p_hat)*1/math.sqrt(sigma_hat_squared)
+    p_value = 1 - scipy.stats.norm.cdf(test_hettmansperger)
+
+    print("Hettmansperger-Norten test")
+    print("Alternative:", alternative)
+    print("Weight:", w)
+    print("test statistic", test_hettmansperger)
+    print("p-value (one-sided)", p_value)
+
+    return None
